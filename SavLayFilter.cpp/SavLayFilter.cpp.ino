bbf838a80d02a8f1587d474320e9eb10be9ed911
James Deromedi <jd1504a@student.american.edu>
2018-09-27 09:26:10 -0400

    Moved from ChaosPendulum_git

---
+++ SavLayFilter.cpp/SavLayFilter.cpp.ino
@@ -0,0 +1,125 @@
+
+#include "Arduino.h"
+#include "SavLayFilter.h"
+
+SavLayFilter::SavLayFilter() {
+  _fillArrayCount = 0;
+}
+
+float SavLayFilter::quadCubicSmoothCalc(int8_t windowSize, float *_toBeSmoothedArray[]) {
+  int8_t i = 0;
+  for (i; i < windowSize; i++) {
+    _sum += (*_toBeSmoothedArray[i] + *_toBeSmoothedArray[(windowSize - 1) - i]) * _quadCubicSmoothConvolute[_arrayPointer][i];
+  }
+  _sum += *_toBeSmoothedArray[(windowSize + 1) / 2] * _quadCubicSmoothConvolute[_arrayPointer][(windowSize + 1) / 2];
+  _sum = _sum / _quadCubicSmoothNormalFactor[_arrayPointer][0];
+
+  return _sum;
+}
+
+float SavLayFilter::quadCubicSmooth(int8_t windowSize, float inputValue) {
+  int8_t j = 1;
+  if (_fillArrayCount != windowSize) {
+    _toBeSmoothedArray[_fillArrayCount] = inputValue;
+    _fillArrayCount++;
+    return 0.0;
+  } else {
+    switch (windowSize) {
+      
+      case 5:
+        _arrayPointer = 0;
+        _smoothedValue = quadCubicSmoothCalc(windowSize, *_toBeSmoothedArray[]);
+        //Moving the data over
+        for (j; j <= windowSize; j++) {
+          *_toBeSmoothedArray[j - 1] = *_toBeSmoothedArray[j];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+        
+      case 7:
+        _arrayPointer = 1;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 9:
+        _arrayPointer = 2;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 11:
+        _arrayPointer = 3;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 13:
+        _arrayPointer = 4;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 15:
+        _arrayPointer = 5;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 17:
+        _arrayPointer = 6;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 19:
+        _arrayPointer = 7;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 21:
+        _arrayPointer = 8;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 23:
+        _arrayPointer = 9;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+      case 25:
+        _arrayPointer = 10;
+        _smoothedValue = quadCubicSmoothCalc( windowSize, _toBeSmoothedArray[]);
+        for (i; i <= windowSize; i++) {
+          *_toBeSmoothedArray[i - 1] = *_toBeSmoothedArray[i];
+        }
+        *_toBeSmoothedArray[windowSize - 1] = inputValue;
+        break;
+    }//END Switch
+    return _smoothedValue;
+  }//END If
+}
+
+
+
---
+++ SavLayFilter.h/SavLayFilter.h.ino
@@ -0,0 +1,49 @@
+#ifndef SavLayFilter_h
+#define SavLayFilter_h
+
+#include "Arduino.h"
+
+class SavLayFilter {
+
+  public:
+    SavLayFilter();
+    float quadCubicSmooth(int8_t windowSize, float inputValue, float *windowArray[]);
+
+  private:
+    float _quadCubicSmoothCalc(int8_t windowSize, float *windowArray[]);
+    int8_t _arrayPointer;
+    int8_t _fillArrayCount;
+    float _sum;
+    float _smoothedValue;
+    float _toBeSmoothedArray[13];
+    int16_t _quadCubicSmoothConvolute[][13] = {
+      { -3, 12, 17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      { -2, 3, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      { -21, 14, 39, 54, 59, 0, 0, 0, 0, 0, 0, 0, 0},
+      { -36, 9, 44, 69, 84, 89, 0, 0, 0, 0, 0, 0, 0},
+      { -11, 0, 9, 16, 21, 24, 25, 0, 0, 0, 0, 0, 0},
+      { -78, -13, 42, 87, 122, 147, 162, 167, 0, 0, 0, 0, 0},
+      { -21, -6, 7, 18, 27, 34, 39, 42, 43, 0, 0, 0, 0},
+      { -136, -51, 24, 89, 144, 189, 224, 249, 264, 269, 0, 0, 0},
+      { -171, -76, 9, 84, 149, 204, 249, 284, 309, 324, 329, 0, 0},
+      { -42, -21, -2, 15, 30, 43, 54, 63, 70, 75, 78, 79},
+      { -254, -138, -33, 62, 147, 222, 287, 322, 387, 422, 447, 462, 467}
+    };
+
+    int16_t _quadCubicSmoothNormalFactor[][1] = {
+      {35},
+      {21},
+      {231},
+      {429},
+      {143},
+      {1105},
+      {323},
+      {2261},
+      {3059},
+      {8059},
+      {5175}
+    };
+
+}//END Class
+
+#endif
