22761bd333549588d5af11c9f28ec3ee335ab1dc
James Deromedi <jd1504a@student.american.edu>
2019-03-12 10:46:06 -0400

    Added two examples, Added more tables in .h, fixed last of the pointers

---
+++ Basic_Example/Basic_Example.ino
@@ -0,0 +1,35 @@
+#include <SavLayFilter.h>
+/**
+   License stuff
+   Author: James Deromedi
+*/
+
+float phase = 0.0;
+float twopi = 3.14159 * 2;
+elapsedMicros usec = 0;
+
+double outputValue;
+int windowsize = 5;
+int convoluteTable = 0;
+
+SavLayFilter smallFilter (&outputValue, 0, 5);             //Cubic smoothing with windowsize of 5
+SavLayFilter largeFilter (&outputValue, 0, 25);            //Cubic smoothing with windowsize of 25
+
+void setup() {
+  Serial.begin(9800);
+}
+
+void loop() {
+  float noise = analogRead(A1);                             //Creates random noise from a floating pin
+  outputValue = (sin(phase) * 1000.0 + 2000.0) + noise;     //Creates sin wave pattern with A = 1000 and shifted up by 2000
+  phase = phase + 0.02;                                     //Shifts the sin wave
+  if (phase >= twopi) phase = 0;                            //Resets the phase
+  while (usec < 30000) ;                                    //Wait to make printing easier to read
+  usec = usec - 30000;                                      //Keeps timer from overflowing
+
+  Serial.print(outputValue);                                //Raw Value [Blue line]
+  Serial.print(",");
+  Serial.print(smallFilter.smoothing());                    //Smoothed value of smaller window [Orange line]
+  Serial.print(",");
+  Serial.println(largeFilter.smoothing());                  //Smoothed value of smaller window [Red line]
+}
--- SavLayFilter/SavLayFilter.cpp
+++ SavLayFilter/SavLayFilter.cpp
@@ -7,195 +7,212 @@
 
 #include <SavLayFilter.h>
 
-SavLayFilter::SavLayFilter(){
-  _fillArrayCount = 0;
+SavLayFilter::SavLayFilter(double* input, int tableNumber, int windowNumber){
+  myInput = input;    //Creates the actual link
+  //myConvoluteTable = tableNumber;   //Saves the table number
+  myWindowSize = windowNumber;    //Saves the window size
+
+  _fillArrayCount = 0;          //Sets all other private variables to zero
   _arrayPointer = 0;
   _sum = 0.0;
   _smoothedValue = 0.0;
 
-}
-
-float SavLayFilter::_calculating(int8_t windowSize, float inputArray[]) {
-  for (int i = 0; i < windowSize/2; i++) {
-    _sum += (inputArray[i] + inputArray[(windowSize - 1) - i]) * (_convoluteTable[_arrayPointer][i+1]);
-  }
-  _sum += inputArray[(windowSize) / 2] * (_convoluteTable[_arrayPointer][((windowSize) / 2)+1]);
-  _sum = (_sum / *_normalizationFactor);
-
-  return _sum;
-}
-
-float SavLayFilter::smoothing(float inputValue, int8_t windowSize, int8_t convoluteTable) {
-  switch (convoluteTable) {								//Meant to save the table, norm factor user specifies
+  switch (tableNumber) {               //Meant to save the convolation table
     case 0:
-    _convoluteTable = _quadCubicSmooth;					//links and saves the table to be used
-    //_normalizationFactor = _quadCubicSmoothNormal;	//Saves which norm factor will be used
-    //Saves the normalization factor that is first column in each table
-    break;	
-    case 1:
-    _convoluteTable =_quadFirstDerive;
-    //_normalizationFactor = _quadFirstDeriveNormal;
-    //_normalizationFactor = _quadFirstDerive[0];
-    break;
-    case 2:
-    _convoluteTable = _quarticQuinticSmooth;
-   //_normalizationFactor = _quadCubicSmoothNormal;
-    //_normalizationFactor = _quarticQuinticSmooth[0];
-    break;
-    case 3:
-    _convoluteTable = _cubicQuarticFirstDerive;
-   // _normalizationFactor = _quadCubicSmoothNormal;
-    break;
-    case 4:
-    _convoluteTable = _quinticFirstDerive;
-   // _normalizationFactor = _quadCubicSmoothNormal;
-    break;
-    case 5:
-    _convoluteTable = _quadCubicSecDerive;
-    //_normalizationFactor = _quadCubicSmoothNormal;
-    break;
-    case 6:
-    _convoluteTable = _quarticQuinticSecDerive;
-    //_normalizationFactor = _quadCubicSmoothNormal;
-    break;
-    case 7:
-    _convoluteTable = _cubicQuarticThirdDerive;
-    //_normalizationFactor = _quadCubicSmoothNormal;
-    break;
-    case 8:
-    _convoluteTable = _quinticThirdDerive;
-    //_normalizationFactor = _quadCubicSmoothNormal;
-    break;
-    case 9:
-    _convoluteTable = _quarticQuinticFourDerive;
-    //_normalizationFactor = _quadCubicSmoothNormal;
-    break;
-    case 10:
-    _convoluteTable = _quinticFifthDerive;
-    //_normalizationFactor = _quadCubicSmoothNormal;
-    break;
-  }
+      _convoluteTable = _quadCubicSmooth;         //links and saves the table to be used
+      break;  
+      case 1:
+      _convoluteTable =_quadFirstDerive;
+      break;
+      case 2:
+      _convoluteTable = _quarticQuinticSmooth;
+      break;
+      case 3:
+      _convoluteTable = _cubicQuarticFirstDerive;
+      break;
+      case 4:
+      _convoluteTable = _quinticFirstDerive;
+      break;
+      case 5:
+      _convoluteTable = _quadCubicSecDerive;
+      break;
+      case 6:
+      _convoluteTable = _quarticQuinticSecDerive;
+      break;
+      case 7:
+      _convoluteTable = _cubicQuarticThirdDerive;
+      break;
+      case 8:
+      _convoluteTable = _quinticThirdDerive;
+      break;
+      case 9:
+      _convoluteTable = _quarticQuinticFourDerive;
+      break;
+      case 10:
+      _convoluteTable = _quinticFifthDerive;
+      break;
+    default:                                //If anything else is given, automatically cubic smoothing
+    _convoluteTable = _quadCubicSmooth;
+  } //End Switch
+}
 
-  if (_fillArrayCount != ((windowSize+1)/2)) {			//Checks if the data array is full
-    _toBeSmoothedArray[_fillArrayCount] = inputValue;	//If not, then it adds it to the array
-    _fillArrayCount++;									//This will cause the first half f window size to be empty
-    return 0.0;
+float SavLayFilter::_calculating(double inputArray[]) {
+  if (_convoluteTable == _quadFirstDerive){
+    for (int i = 0; i < myWindowSize/2; i++) {
+      _sum += (inputArray[i]) * (_convoluteTable[_arrayPointer][i+1]);
+    }
+    for (int i = (myWindowSize/2 + 1); i < myWindowSize; i++) {
+      _sum += (inputArray[i]) * (-1 * _convoluteTable[_arrayPointer][(i - (myWindowSize/2 + 1)) + 1]);
+    }
+    _sum += inputArray[(myWindowSize) / 2] * (_convoluteTable[_arrayPointer][((myWindowSize) / 2)+1]);
+    _sum = (_sum / *_normalizationFactor);
+
+    return _sum;
+  } else {
+
+    for (int i = 0; i < myWindowSize/2; i++) {
+      _sum += (inputArray[i] + inputArray[(myWindowSize - 1) - i]) * (_convoluteTable[_arrayPointer][i+1]);
+    }
+    _sum += inputArray[(myWindowSize) / 2] * (_convoluteTable[_arrayPointer][((myWindowSize) / 2)+1]);
+    _sum = (_sum / *_normalizationFactor);
+
+    return _sum;
+  }
+}
+
+float SavLayFilter::smoothing() {
+  if (_fillArrayCount != ((myWindowSize+1)/2)) {		  	//Checks if the data array is full
+    _toBeSmoothedArray[_fillArrayCount] = *myInput;	   //If not, then it adds it to the array
+    _fillArrayCount++;									              //This will cause the first half f window size to be empty
+    _smoothedValue = 0.0;
+    return _smoothedValue;
 
   } else {
-    switch (windowSize) {						//Calls the calculation function on the windowsize set by user
+    switch (myWindowSize) {						//Calls the calculation function on the windowsize set by user
 
       case 5:
-      _arrayPointer = 0;						//Sets which line to call in the array for window size values
-      _normalizationFactor = _convoluteTable[0, 0];			//Saves the norm factor for the windowsize/convolute table
-      _smoothedValue = _calculating(windowSize, _toBeSmoothedArray);	//Computes a smooth value
-      for (int j = 1; j <= windowSize; j++) {							// moves window over input array
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;					//Inputs newly smoothed value
-      break;
+        _arrayPointer = 0;						//Sets which line to call in the array for window size values
+        _normalizationFactor = _convoluteTable[_arrayPointer];			              //Saves the norm factor for the windowsize/convolute table
+        _smoothedValue = _calculating( _toBeSmoothedArray);	            //Computes a smooth value
 
-      case 7:
-      _arrayPointer = 1;
-      _normalizationFactor = _convoluteTable[1, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        for (int j = 1; j <= myWindowSize; j++) {						            //moves window over input array
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;				      	//Inputs newly smoothed value
+        break;
 
-      case 9:
-      _arrayPointer = 2;
-      _normalizationFactor = _convoluteTable[2, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        case 7:
+        _arrayPointer = 1;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating( _toBeSmoothedArray);
 
-      case 11:
-      _arrayPointer = 3;
-      _normalizationFactor = _convoluteTable[3, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
 
-      case 13:
-      _arrayPointer = 4;
-      _normalizationFactor = _convoluteTable[4, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        case 9:
+        _arrayPointer = 2;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating(_toBeSmoothedArray);
 
-      case 15:
-      _arrayPointer = 5;
-      _normalizationFactor = _convoluteTable[5, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
 
-      case 17:
-      _arrayPointer = 6;
-      _normalizationFactor = _convoluteTable[6, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        case 11:
+        _arrayPointer = 3;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating(_toBeSmoothedArray);
 
-      case 19:
-      _arrayPointer = 7;
-      _normalizationFactor = _convoluteTable[7, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
 
-      case 21:
-      _arrayPointer = 8;
-      _normalizationFactor = _convoluteTable[8, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        case 13:
+        _arrayPointer = 4;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating( _toBeSmoothedArray);
 
-      case 23:
-      _arrayPointer = 9;
-      _normalizationFactor = _convoluteTable[9, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
 
-      case 25:
-      _arrayPointer = 10;
-      _normalizationFactor = _convoluteTable[10, 0];
-      _smoothedValue = _calculating( windowSize, _toBeSmoothedArray);
-      for (int j = 1; j <= windowSize; j++) {
-        _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
-      }
-      _toBeSmoothedArray[windowSize - 1] = inputValue;
-      break;
+        case 15:
+        _arrayPointer = 5;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating( _toBeSmoothedArray);
 
-      default:
-      _smoothedValue = -9999999;
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
+
+        case 17:
+        _arrayPointer = 6;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating( _toBeSmoothedArray);
+
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
+
+        case 19:
+        _arrayPointer = 7;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating( _toBeSmoothedArray);
+
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
+
+        case 21:
+        _arrayPointer = 8;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating( _toBeSmoothedArray);
+
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
+
+        case 23:
+        _arrayPointer = 9;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating( _toBeSmoothedArray);
+
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
+
+        case 25:
+        _arrayPointer = 10;
+        _normalizationFactor = _convoluteTable[_arrayPointer];
+        _smoothedValue = _calculating( _toBeSmoothedArray);
+
+        for (int j = 1; j <= myWindowSize; j++) {
+          _toBeSmoothedArray[j - 1] = _toBeSmoothedArray[j];
+        }
+        _toBeSmoothedArray[myWindowSize - 1] = *myInput;
+        break;
+
+        default:
+        _smoothedValue = -9999999;
 
     }//END Switch
     //testingArray[0] = _arrayPointer;
